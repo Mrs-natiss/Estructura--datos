e4963fe27bf1ef993b394d10cd70366df5de1278
nataliaafanador56 <nataliaafanador56@gmail.com>
2024-08-10 17:30:12 +0000

    nueva actualizacion

--- inicio.py
+++ inicio.py
@@ -1,1 +1,8 @@
 edad = 22
+edad = int(input("ingrese su edad"))
+# crear una condicion donde los menores de 18 no puedan entrar 
+if edad < 18:
+    print("no puedes entrar care monda")
+else:
+    print("puedes entrar")
+    
