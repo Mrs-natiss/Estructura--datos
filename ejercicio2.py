55d66129060bd1a8f9a0c433668cdad595f95375
nathaliafanador13 <nathaliafanador13@gmail.com>
2024-08-24 03:34:57 +0000

    Calcular operaciones

--- ejercicio2.py
+++ ejercicio2.py
@@ -1,10 +1,8 @@
-#Solicitar si el numero es par o impar
 numero=int(input("Ingrese un numero: "))
 if numero%2==0:
     print("El numero es par")
 else:
     print("El numero es impar")
 
-#Mostrar el resultado 
 print("El numero es par" if numero%2==0 else "El numero es impar")
 
