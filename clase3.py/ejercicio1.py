0fcf5d1c98841ad7df1e3c596090e42b4a7b1385
Mrs-natiss <nathaliafanador13@gmail.com>
2024-08-31 15:58:01 +0000

    calculadore

---
+++ clase3.py/ejercicio1.py
@@ -0,0 +1,10 @@
+n = int(input("Ingrese el primer número "))
+k = int(input("Ingrese el segundo número "))
+suma = n + k
+resta = n - k
+multiplicacion = n * k
+division = n/k
+print(f"La suma es {suma}")
+print(f"la resta es {resta}")
+print(f"la multiplicación es {multiplicacion}")
+print(f"la división es {division}")
